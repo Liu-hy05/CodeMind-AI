2452fa5c545f8d9d69df14c2a391814513481162
Liu <3314352933@qq.com>
2026-09-10 20:59:41 +0800

    feat: complete database persistence for user module

--- backend/codemind-user/src/main/java/com/codemind/user/mapper/UserMapper.java
+++ backend/codemind-user/src/main/java/com/codemind/user/mapper/UserMapper.java
@@ -1,13 +1,41 @@
 package com.codemind.user.mapper;
 
 import com.codemind.user.entity.User;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserMapper {
 
+
+    @Select("""
+            SELECT *
+            FROM user
+            WHERE username = #{username}
+            """)
     User selectByUsername(String username);
 
+
+    @Options(
+            useGeneratedKeys = true,
+            keyProperty = "id"
+    )
+    @Insert("""
+        INSERT INTO user
+        (
+            username,
+            password,
+            role
+        )
+        VALUES
+        (
+            #{username},
+            #{password},
+            #{role}
+        )
+        """)
     int insert(User user);
 
 }
